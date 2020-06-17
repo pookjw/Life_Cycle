82585fb6b49e1b4c2c61e3429f2920881f729c9e
pookjw <kidjinwoo@me.com>
2020-06-17 11:03:10 +0900

    Initial Commit

---
+++ Life_Cycle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Life_Cycle
+//
+//  Created by pook on 6/17/20.
+//  Copyright © 2020 jinwoopeter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
